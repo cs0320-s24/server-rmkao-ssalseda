3b91ae99e540371380f1e0c25fccdece2433ac5f
rayraytheray <raymond_kao@brown.edu>
2024-02-13 13:31:07 -0500

    reformatting

--- src/main/java/edu/brown/cs/student/main/broadCode/BroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/broadCode/BroadbandHandler.java
@@ -1,11 +1,7 @@
 package edu.brown.cs.student.main.broadCode;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import edu.brown.cs.student.main.server.Datasource;
+import edu.brown.cs.student.main.server.MockedDatasource;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -14,79 +10,31 @@ import spark.Response;
 import spark.Route;
 
 public class BroadbandHandler implements Route {
-// must return data given state and county parameters
+  // must return data given state and county parameters
 
-  //  To keep the format simple, all responses must be serializations of a Map<String, Object> object.
-//  For all replies, the map must contain a "result" field with value "success" in case of success
-//  or an error code in the case of an error: (see sprint doc)
-  /**
-   * This handle method needs to be filled by any class implementing Route. When the path set in
-   * edu.brown.cs.examples.moshiExample.server.Server gets accessed, it will fire the handle method.
-   *
-   * <p>NOTE: beware this "return Object" and "throws Exception" idiom. We need to follow it because
-   * the library uses it, but in general this lowers the protection of the type system.
-   *
-   * @param request The request object providing information about the HTTP request
-   * @param response The response object providing functionality for modifying the response
-   */
+  //  To keep the format simple, all responses must be serializations of a Map<String, Object>
+  // object.
+  //  For all replies, the map must contain a "result" field with value "success" in case of success
+  //  or an error code in the case of an error: (see sprint doc)
+  private Datasource dataSource = new MockedDatasource();
+
   @Override
   public Object handle(Request request, Response response) {
-    // If you are interested in how parameters are received, try commenting out and
-    // printing these lines! Notice that requesting a specific parameter requires that parameter
-    // to be fulfilled.
-    // If you specify a queryParam, you can access it by appending ?parameterName=name to the
-    // endpoint
-    // ex. http://localhost:3232/activity?participants=num
     Set<String> params = request.queryParams();
-    //     System.out.println(params);
-    String participants = request.queryParams("participants");
-    //     System.out.println(participants);
+    System.out.println(params);
+    String county = request.queryParams("county");
+    System.out.println(county);
+    String state = request.queryParams("state");
+    System.out.println(state);
 
     // Creates a hashmap to store the results of the request
     Map<String, Object> responseMap = new HashMap<>();
     try {
-      // Sends a request to the API and receives JSON back
-      String activityJson = this.sendRequest();
-      // Deserializes JSON into an Activity
-      Activity activity = ActivityAPIUtilities.deserializeActivity(activityJson);
-      // Adds results to the responseMap
-      responseMap.put("result", "success");
-      responseMap.put("activity", activity);
-      return responseMap;
+      Map<String, Object> response = dataSource.getData();
     } catch (Exception e) {
       e.printStackTrace();
-      // This is a relatively unhelpful exception message. An important part of this sprint will be
-      // in learning to debug correctly by creating your own informative error messages where Spark
-      // falls short.
       responseMap.put("result", "Exception");
     }
     return responseMap;
   }
-
-  private String sendRequest() throws URISyntaxException, IOException, InterruptedException {
-    // Build a request to this BoredAPI. Try out this link in your browser, what do you see?
-    // TODO 1: Looking at the documentation, how can we add to the URI to query based
-    // on participant number?
-    HttpRequest buildBoredApiRequest =
-        HttpRequest.newBuilder()
-            .uri(new URI("http://www.boredapi.com/api/activity/"))
-            .GET()
-            .build();
-
-    // Send that API request then store the response in this variable. Note the generic type.
-    HttpResponse<String> sentBoredApiResponse =
-        HttpClient.newBuilder()
-            .build()
-            .send(buildBoredApiRequest, HttpResponse.BodyHandlers.ofString());
-
-    // What's the difference between these two lines? Why do we return the body? What is useful from
-    // the raw response (hint: how can we use the status of response)?
-    System.out.println(sentBoredApiResponse);
-    System.out.println(sentBoredApiResponse.body());
-
-    return sentBoredApiResponse.body();
-  }
-
-
-
 }
