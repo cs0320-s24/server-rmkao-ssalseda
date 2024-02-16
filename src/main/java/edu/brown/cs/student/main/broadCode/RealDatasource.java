4f46d3ff188d3b3351899e2723a7483d4f94d6b8
“samsalseda” <samuel_salseda@brown.edu>
2024-02-15 23:22:15 -0500

    saving basic model of caching before I try to change it entirely

--- src/main/java/edu/brown/cs/student/main/broadCode/RealDatasource.java
+++ src/main/java/edu/brown/cs/student/main/broadCode/RealDatasource.java
@@ -1,5 +1,8 @@
 package edu.brown.cs.student.main.broadCode;
 
+import edu.brown.cs.student.main.cache.CachedFilePager;
+import edu.brown.cs.student.main.cache.FilePager;
+import edu.brown.cs.student.main.cache.Pager;
 import edu.brown.cs.student.main.server.Datasource;
 import java.io.IOException;
 import java.net.URI;
@@ -7,6 +10,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
 
 public class RealDatasource implements Datasource {
   @Override
@@ -45,19 +49,28 @@ public class RealDatasource implements Datasource {
 
   @Override
   public String getBroadband(String county, String state)
-      throws URISyntaxException, IOException, InterruptedException {
+      throws URISyntaxException, IOException, InterruptedException, ExecutionException {
     String constructedString =
         "https://api.census.gov/data/2021/acs/acs1/subject/variables?get=NAME,S2802_C03_022E&for=county:"
             + county
             + "&in=state:"
             + state;
-    HttpRequest buildApiRequest =
-        HttpRequest.newBuilder().uri(new URI(constructedString)).GET().build();
+//    HttpRequest buildApiRequest =
+//        HttpRequest.newBuilder().uri(new URI(constructedString)).GET().build();
 
     // Send that API request then store the response in this variable. Note the generic type.
-    HttpResponse<String> sentApiResponse =
-        HttpClient.newBuilder().build().send(buildApiRequest, HttpResponse.BodyHandlers.ofString());
+    return new CachedFilePager(new FilePager(constructedString)).pager(constructedString).body();
+    //    new CachedFilePager(HttpClient.newBuilder().build()
+    //        .send(buildApiRequest, HttpResponse.BodyHandlers.ofString()));
 
-    return sentApiResponse.body();
   }
+
+  public String process(Pager<String, HttpResponse<String>> pag, String s)
+      throws IOException, URISyntaxException, ExecutionException, InterruptedException {
+    return
+        pag.pager(s).body();
+  }
+
+
+
 }
