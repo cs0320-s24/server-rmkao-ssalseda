03b7ee83c0ebcd985cbbb0729865dd854a761e0f
rayraytheray <raymond_kao@brown.edu>
2024-02-17 17:58:44 -0500

    Datasource cleanup

--- src/main/java/edu/brown/cs/student/main/broadCode/RealDatasource.java
+++ src/main/java/edu/brown/cs/student/main/broadCode/RealDatasource.java
@@ -10,42 +10,6 @@ import java.net.http.HttpResponse;
 public class RealDatasource implements Datasource {
   public RealDatasource() {}
 
-  //  @Override
-  //  public String getStateCodes() throws URISyntaxException, IOException, InterruptedException {
-  //    HttpRequest buildApiRequest =
-  //        HttpRequest.newBuilder()
-  //            .uri(new URI("https://api.census.gov/data/2010/dec/sf1?get=NAME&for=state:*"))
-  //            .GET()
-  //            .build();
-  //
-  //    // Send that API request then store the response in this variable. Note the generic type.
-  //    HttpResponse<String> sentApiResponse =
-  //        HttpClient.newBuilder().build().send(buildApiRequest,
-  // HttpResponse.BodyHandlers.ofString());
-  //
-  //    return sentApiResponse.body();
-  //  }
-  //
-  //  @Override
-  //  public String getCountyCodes(String state)
-  //      throws URISyntaxException, IOException, InterruptedException {
-  //    HttpRequest buildApiRequest =
-  //        HttpRequest.newBuilder()
-  //            .uri(
-  //                new URI(
-  //                    "https://api.census.gov/data/2010/dec/sf1?get=NAME&for=county:*&in=state:"
-  //                        + state))
-  //            .GET()
-  //            .build();
-  //
-  //    // Send that API request then store the response in this variable. Note the generic type.
-  //    HttpResponse<String> sentApiResponse =
-  //        HttpClient.newBuilder().build().send(buildApiRequest,
-  // HttpResponse.BodyHandlers.ofString());
-  //
-  //    return sentApiResponse.body();
-  //  }
-
   @Override
   public String getData(String constructedString)
       throws URISyntaxException, IOException, InterruptedException {
